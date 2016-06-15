8ad3a92564ea5676f8c999ba24919605c96059f5
assunta09 <assunta.crux@gmail.com>
2016-06-15 11:58:15 +0200

    add questions to the file

---
+++ ruby/vampires.rb
@@ -0,0 +1,20 @@
+puts "What is your name?"
+
+name = gets.chomp
+
+puts "How old are you?"
+
+age = gets.chomp
+
+puts "What yeasr were you born?"
+
+year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+
+menue = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+
+insurance = gets.chomp
+
