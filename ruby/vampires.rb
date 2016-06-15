cbb344148d21570e39f52425211b4df2fddb7076
assunta09 <assunta.crux@gmail.com>
2016-06-15 19:27:54 +0200

    add loops to identify vampires

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,32 +1,56 @@
-puts "What is your name?"
+puts "How many candidates will be screened?"
+number_users = gets.to_i
 
-name = gets.chomp
+until number_users == 0
+	puts "What is your name?"
+ 	name = gets.chomp
 
-puts "How old are you?"
+	puts "How old are you?"
+	age = gets.to_i
 
-age = gets.to_i
+	puts "What year were you born?"
+	year = gets.to_i
 
-puts "What yeasr were you born?"
+	puts "Our company cafeteria serves garlic bread. Should we order some for you (y/n)?"
+	menue = gets.chomp
 
-year = gets.to_i
+	puts "Would you like to enroll in the company's health insurance (y/n)?"
+	insurance = gets.chomp
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you (y/n)?"
+#while loop to request alergies. If the candidate is allergic to sunshine the loop is skipped and the result printed
+	allergy = "open"
 
-menue = gets.chomp
+	while allergy != "done"
+		puts "Please list your allergies one by one. When you are finished enter done"
+		allergy = gets.chomp
+		if allergy == "sunshine"
+			result = "Probably a vampire"
+			allergy = "done"
+		end
+	end
 
-puts "Would you like to enroll in the company's health insurance (y/n)?"
+	if result == "Probably a vampire"
+		puts result
 
-insurance = gets.chomp
+#if the allergie test did not render a vampire. The requested information is evaluated		
+	else	
+		if name == "Drake Cula" || name == "Tu Fang"
+			result = "Definitively a vampire"
+		elsif (2016-year) == age && menue == "yes" && insurance == "yes"
+			result = "Probably not a vampire"
+		elsif (2016-year) != age && menue != "yes" && insurance != "yes"
+			result = "Almost certainly a vampire"
+		elsif (2016-year) != age && menue != "yes" || insurance != "yes"
+			result = "Probably a vampire"
+		else
+			result = "results inconclusive"
+		end
+		puts result
+	end
 
+	number_users = number_users -1
 
-if name == "Drake Cula" || name == "Tu Fang"
-	puts "Definitively a vampire"
-elsif (2016-year) == age && menue == "yes" && insurance == "yes"
-	puts "Probably not a vampire"
-elsif (2016-year) != age && menue != "yes" && insurance != "yes"
-	puts "Almost certainly a vampire"
-elsif (2016-year) != age && menue != "yes" || insurance != "yes"
-	puts "Probably a vampire"
-else
-	puts "results inconclusive"
-end
+end	
+
+puts "All candidates screened"
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends"
