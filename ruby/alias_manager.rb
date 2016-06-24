38ff4ddb50c3b6bd346337086f21779179f740c8
assunta09 <assunta.crux@gmail.com>
2016-06-24 19:36:13 +0200

    add release 0 to program

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,74 +1,92 @@
 puts "SPYINATOR 2000 - BULLETPROOF SECRET AGENT NAMES"
-
-# request spy name
-puts "What is you name"
-# store spy name in variable
-spyinator = gets.chomp
-# turn spy names into array consisting of the names
-
-spyinator_1 = spyinator.split(' ')
+stop = " "
+time = 0
+spy_user = {}
 
 
-# swapp first and second name
 
-spyinator_1[0],spyinator_1[-1] = spyinator_1[-1], spyinator_1[0]
+while stop != "quit"
+	# request spy name
+	puts "Enter a name you want to have converted"
+	# store spy name in variable
+	spyinator = gets.chomp
+	# turn spy names into array consisting of the names
 
-# put it back into a string
+	spyinator_1 = spyinator.split(' ')
 
-spy_stage_1 = spyinator_1.join(' ')
+	# swapp first and second name
+
+	spyinator_1[0],spyinator_1[-1] = spyinator_1[-1], spyinator_1[0]
+
+	# incase the user has entered some of the name lowercase - capitalize
+
+	spyinator_1.map!(&:capitalize)
+
+	# put it back into a string
+
+	spy_stage_1 = spyinator_1.join(' ')
 
 
-# turn spy names into array consisting of the letters
-spyinator_2 = spy_stage_1.split('')
+	# turn spy names into array consisting of the letters
+	spyinator_2 = spy_stage_1.split('')
 
 
-# exchange the vowels for the next vowel in 'aeiou'
-# exchange the consonants for the next consonant in the alphabet
+	# exchange the vowels for the next vowel in 'aeiou'
+	# exchange the consonants for the next consonant in the alphabet
 
-
-count = 0
-spyinator_2.each { |spy|
-	if "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".index(spy) != nil
-		if "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".index(spy) == 31
-			spy = "B"
-#			spyinator_2[count] = spy
-		elsif "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".index(spy) == 19
-			new_spy = "b"
-#			spyinator_2[count] = spy
-		else
-			spy_index = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".index(spy) + 1
-			spy = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"[spy_index]
-#			spyinator_2[count] = spy
+	count = 0
+	spyinator_2.each { |spy|
+		if "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".index(spy) != nil
+			if "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".index(spy) == 31
+				spy = "B"
+			elsif "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".index(spy) == 19
+				new_spy = "b"
+			else
+				spy_index = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".index(spy) + 1
+				spy = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"[spy_index]
+			end
+		elsif spy == " "
+			spy = " "
+		else 
+			if "aeiouAEIOU".index(spy) == 4
+				spy = "a"
+			elsif "aeiouAEIOU".index(spy) == 9
+				spy = "A"
+			else
+				spy_index = "aeiouAEIOU".index(spy) + 1
+				spy = "aeiouAEIOU"[spy_index]
+			end
 		end
-	elsif spy == " "
-		spy = " "
-#		spyinator_2[count] = spy
-	else 
-		if "aeiouAEIOU".index(spy) == 4
-			spy = "a"
-#			spyinator_2[count] = spy
-		elsif "aeiouAEIOU".index(spy) == 9
-			spy = "A"
-#			spyinator_2[count] = spy
-		else
-			spy_index = "aeiouAEIOU".index(spy) + 1
-			spy = "aeiouAEIOU"[spy_index]
-#			spyinator_2[count] = spy
-		end
-	end
-	spyinator_2[count] = spy
-	count += 1
-}
 
-#join the letters back into a string
-spyinator_finale = spyinator_2.join('')
+		spyinator_2[count] = spy
+		count += 1
+	}
 
 
-puts "Congrats your new name is #{spyinator_finale}"
+	#join the letters back into a string
+	spyinator_finale = spyinator_2.join('')
+	
+	time += 1
+	test = "spyinator_".concat(time.to_s)
+	spy_user[test] = {
+		:name => spyinator,
+		:spyname => spyinator_finale
+		}
+
+	p spy_user
 
 
+	puts "The spyname for #{spyinator} is #{spyinator_finale}
 
-# exchange the consonants for the next consonant in the alphabet
+Hit enter to test another name or type quit to stop the programm"
+
+	stop = gets.chomp
+end
+
+
+spy_user.each{|key| puts key }
+
+
 
 
 
