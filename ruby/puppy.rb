ca71d2a7e6fb1f8de49b01466337d68f07719e18
assunta09 <assunta.crux@gmail.com>
2016-06-28 11:28:07 +0200

    design puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+# Class: Puppies
+	# Instances: 
+		# - Puppy Name_1
+		# - Puppy Name_2
+		# - ...
+
+# Characteristics
+	# 	Fur: fluffy
+	# 	Fur Length: varies
+	#   Friendly: yes
+	#   Name: varies
+	# 	Color: varies
+
+# Behavior
+	# Jump
+	# Bark
+	# Cuddle
+	# Sleep
+	# Eat
+	
