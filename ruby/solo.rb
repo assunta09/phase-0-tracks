369466e108f4b4a6f2c129b3c388e902b0ff3179
assunta09 <assunta.crux@gmail.com>
2016-06-30 09:40:59 +0200

    create pseudocode

---
+++ ruby/solo.rb
@@ -0,0 +1,16 @@
+# design a class called summer
+	# initialize the class
+	# the class has the following attributes:
+		# - heat: integer
+		# - weather: string
+		# - things to wear
+	# the class has three methods
+
+		# setter methods
+			# - takes the heat and tells the felt heat which is about 10 degree hotter
+			# - tells the weather and says what to wear
+		# normal method
+			# - tells the user way to stay
+
+			
+
